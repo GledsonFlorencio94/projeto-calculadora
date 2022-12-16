de3f9a568f0345f37a57ae8c0b69bff7c752a06d
GledsonFlorencio94 <gledsonflorencio@gmail.com>
2022-12-16 11:25:41 -0300

    criando input e buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import Input from './components/Input';
+import Button from './components/Button';
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
 
 
-function App() {
+
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  
+  const handleAddNumber = (num) => {
+      setCurrentNumber(prev => `${num}${prev}`)
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <Content>
+      <Input value={currentNumber} />
+      <Row>
+        <Button label="x" onClick={() => handleAddNumber('x')}  />
+        <Button label="/" onClick={() => handleAddNumber('/')}  />
+        <Button label="c" onClick={() => handleAddNumber('c')}  />
+        <Button label="x" onClick={() => handleAddNumber('x')}  />
+      </Row> 
+      <Row>
+        <Button label="7" onClick={() => handleAddNumber('7')}  />
+        <Button label="8" onClick={() => handleAddNumber('8')}  />
+        <Button label="9" onClick={() => handleAddNumber('9')}  />
+        <Button label="-" onClick={() => handleAddNumber('')}  />
+      </Row> 
+      <Row>
+        <Button label="4" onClick={() => handleAddNumber('4')}  />
+        <Button label="5" onClick={() => handleAddNumber('5')}  />
+        <Button label="6" onClick={() => handleAddNumber('6')}  />
+        <Button label="+" onClick={() => handleAddNumber('')}  />
+      </Row> 
+      <Row>
+        <Button label="1" onClick={() => handleAddNumber('1')}  />
+        <Button label="2" onClick={() => handleAddNumber('2')}  />
+        <Button label="3" onClick={() => handleAddNumber('3')}  />
+        <Button label="=" onClick={() => handleAddNumber('=')} />
+      </Row> 
+      </Content>
+    </Container>
   );
 }
 
